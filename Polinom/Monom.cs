afe574f839b2c0cf1e0bb5a9de69f1e62823e3b4
bianes <andbihari@gmail.com>
2018-11-21 11:32:22 +0100

    Code style

--- Polinom/Monom.cs
+++ Polinom/Monom.cs
@@ -8,36 +8,33 @@ namespace ClsPolinom
 {
     public class Monom
     {
-        private long _coefficient;
-        private string _variable;
-        private ulong _exponent;
-
-
         public Monom() { }
 
-        public Monom(long Coefficient, string Variable, ulong Exponent) {
-            this._coefficient = Coefficient;
-            this._exponent = Exponent;
-            this._variable = Variable;
+        public Monom(long Coefficient, string Variable, ulong Exponent)
+        {
+            this.Coefficient = Coefficient;
+            this.Exponent = Exponent;
+            this.Variable = Variable;
         }
 
         //one variable polinom
         public Monom(long Coefficient, ulong Exponent)
         {
-            this._coefficient = Coefficient;
-            this._exponent = Exponent;
-            this._variable = "x";
+            this.Coefficient = Coefficient;
+            this.Exponent = Exponent;
+            this.Variable = "x";
         }
 
         //Constant polinom 
-        public Monom(long Coefficient) {
-            this._coefficient = Coefficient;
+        public Monom(long Coefficient)
+        {
+            this.Coefficient = Coefficient;
             this.Exponent = 0;
             this.Variable = "x";
         }
 
-        public long Coefficient { get => _coefficient; set => _coefficient = value; }
-        public string Variable { get => _variable; set => _variable = value; }
-        public ulong Exponent { get => _exponent; set => _exponent = value; }
+        public long Coefficient { get; set; }
+        public string Variable { get; set; }
+        public ulong Exponent { get; set; }
     }
 }
